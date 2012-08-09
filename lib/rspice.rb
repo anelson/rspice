9f15807e0d0f506a73320cca3ca16169816dd5de
Adam Nelson <anelson@apocryph.org>
2012-08-09 08:30:24 -0400

    Refactored a bit, introducing SpiceVector class

--- lib/rspice.rb
+++ lib/rspice.rb
@@ -5,14 +5,12 @@ end
 require 'cspice_wrapper'
 
 require "rspice/version"
-require "rspice/spice_double_array"
+require "rspice/spice_vector"
 require "rspice/cspice"
 require "rspice/cspice_error"
 require "rspice/kernels"
 require "rspice/time"
 require "rspice/ephemeris"
-require "rspice/body_position"
-require "rspice/body_velocity"
 require "rspice/body_state"
 
 
